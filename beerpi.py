7d56aee61d415ab32ef25f71ca12e868e760327b
Rutger Kerkhoff <rutger@rhkerkhoff.com>
2020-07-20 21:25:04 +0200

    Initial version
    Not yet tested on the pi, only on a PC without GPIO/sensors connected

---
+++ beerpi.py
@@ -0,0 +1,61 @@
+import RPi.GPIO as GPIO
+import paho.mqtt.client as mqtt
+import logging
+import time
+
+# These are files
+from config import *
+import utils
+
+
+def on_connect(client, userdata, flags, rc):
+    logging.info("Connected with result code " + str(rc))
+    # With the # suffix we subscribe to all subtopics
+    client.subscribe(MQTT_RELAY_READ + "#")
+
+
+def on_disconnect(client, userdata, rc=0):
+    logging.info("Disconnected with result code " + str(rc))
+    client.loop_stop()
+
+
+def on_message(client, userdata, msg):
+    logging.info(msg.topic + " " + str(msg.payload))
+
+    sensor_nr = msg.topic.split(MQTT_RELAY_READ)[1]
+    if sensor_nr == FRIDGE_HEAT_TOPIC:
+        pin = HEAT_GPIO
+    elif sensor_nr == FRIDGE_COOL_TOPIC:
+        pin = COOL_GPIO
+    else:
+        logging.error("Unknown topic")
+        return
+
+    if msg.payload == RELAY_OFF_PAYLOAD:
+        pinstate = GPIO.HIGH
+    elif msg.payload == RELAY_ON_PAYLOAD:
+        pinstate = GPIO.LOW
+    else:
+        logging.error("Unknown payload")
+        return
+
+    GPIO.output(pin, pinstate)
+    logging.info("Set pin " + str(pin) + " to " + str(pinstate))
+    client.publish(MQTT_RELAY_PUBLISH + sensor_nr, msg.payload)
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+client.on_message = on_message
+client.connect(MQTT_HOST, MQTT_PORT, 60)
+
+# Non-blocking loop
+client.loop_start()
+
+# So we can loop forever
+while True:
+    temperature = read_temp()
+    logging.info("Temp: " + str(temperature))
+    client.publish(TEMP_MQTT_TOPIC, str(temperature))
+    time.sleep(TEMP_READ_INTERVAL)
---
+++ config.py
@@ -0,0 +1,27 @@
+import logging
+
+# Debug level of info prints the temperature every minute, might be
+# too much data
+logging.getLogger().setLevel(logging.INFO)
+
+# Currently no authentication on client level implemented
+# Should be really easy to add
+MQTT_HOST = "10.80.20.10"
+MQTT_PORT = 1883
+# We end both with a slash, and append the string of the topics below
+# (FRIDGE_COOL_TOPIC and FRIDGE_HEAT_TOPIC) for the specific topics
+MQTT_RELAY_READ = "raspberry/switch/"
+MQTT_RELAY_PUBLISH = "raspberry/switch_state/"
+
+# How often to read the temperature and send to mqtt server
+# In seconds
+TEMP_READ_INTERVAL = 60
+TEMP_MQTT_TOPIC = "raspberry/temperature/1"
+
+# The settings for the two relays
+COOL_GPIO = 1
+HEAT_GPIO = 2
+FRIDGE_COOL_TOPIC = "2"
+FRIDGE_HEAT_TOPIC = "1"
+RELAY_OFF_PAYLOAD = b"1"
+RELAY_ON_PAYLOAD = b"0"
