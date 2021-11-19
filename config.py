802005db941b4a19594e834b2a8e62cf05b9e0a1
Rutger Kerkhoff <rutger@rhkerkhoff.com>
2021-11-19 16:29:02 +0000

    Initial PT100 support
    This is not tested completely.

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import logging
+import board
 
 # Debug level of info prints the temperature every minute, might be
 # too much data
@@ -6,7 +7,7 @@ logging.getLogger().setLevel(logging.INFO)
 
 # Currently no authentication on client level implemented
 # Should be really easy to add
-MQTT_HOST = "10.80.30.10"
+MQTT_HOST = "10.80.30.11"
 MQTT_PORT = 1883
 # We end both with a slash, and append the string of the topics below
 # (FRIDGE_COOL_TOPIC and FRIDGE_HEAT_TOPIC) for the specific topics
@@ -17,6 +18,10 @@ MQTT_RELAY_PUBLISH = "raspberry/switch_state/"
 # In seconds
 TEMP_READ_INTERVAL = 60
 TEMP_MQTT_TOPIC = "raspberry/temperature/1"
+BEER_TEMP_MQTT_TOPIC = "raspberry/temperature/2"
+
+# Pin for PT100
+PT100_PIN = board.D5
 
 # The settings for the two relays
 COOL_GPIO = 20
